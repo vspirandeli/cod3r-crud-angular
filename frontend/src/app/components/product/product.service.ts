2ac19cc4e52c9d5f37f5c8920f1ce1661168bcba
Victor Spirandeli <vspirandeli@gmail.com>
2023-01-03 12:07:13 -0300

    Add: Tratativa de erros simples

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Product } from './product-model';
 
 @Injectable({
@@ -12,16 +12,26 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError = false): void {
     this.snackBar.open(msg, 'X', {
-      duration: 3000,
+      duration: 300000,
       horizontalPosition: 'right',
       verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
     });
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.handleError(e))
+    );
+  }
+
+  handleError(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro', true);
+
+    return EMPTY;
   }
 
   read(): Observable<Product[]> {
