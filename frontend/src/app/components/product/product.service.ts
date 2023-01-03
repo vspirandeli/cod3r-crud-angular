3e479ef039d37f7c70c73c12efd6ecaae997a408
Victor Spirandeli <vspirandeli@gmail.com>
2023-01-03 10:52:49 -0300

    Add: Criado os serviços do projeto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -5,22 +5,19 @@ import { Observable } from 'rxjs';
 import { Product } from './product-model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  baseUrl: string = 'http://localhost:3001/products';
+  baseUrl = 'http://localhost:3001/products';
 
-  constructor(
-    private snackBar: MatSnackBar,
-    private http: HttpClient
-  ) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
-    })
+      verticalPosition: 'top',
+    });
   }
 
   create(product: Product): Observable<Product> {
@@ -29,5 +26,17 @@ export class ProductService {
 
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+
+    return this.http.get<Product>(url);
+  }
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+
+    return this.http.put<Product>(url, product);
   }
 }
